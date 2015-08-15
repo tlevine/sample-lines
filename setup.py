670d7e10626a2d02377e9741dc44890695687bd0
Thomas Levine <_@thomaslevine.com>
2015-08-15 16:23:26 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(name='sample-lines',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Sample lines from a file.',
+      url='http://dada.pink/sample-lines/',
+      py_modules=['sample_lines'],
+      version='0.0.1',
+      license='LGPL',
+      entry_points = {'console_scripts': ['sample_lines = sample_lines:cli']},
+)
