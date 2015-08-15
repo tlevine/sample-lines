d4ce858e0a89353f6ff4d2405013d8df599394a5
Thomas Levine <_@thomaslevine.com>
2015-08-15 16:12:44 -0400

    systematic

--- sample_lines.py
+++ sample_lines.py
@@ -22,4 +22,20 @@ def srs(n, fp):
         if lines_emitted == 0 and i > 1000:
             raise EnvironmentError('It appears that this file contains no line breaks.')
 
-    return lines
+def systematic(n, fp, offset = 0, repetitions = 1):
+    file_start = fp.tell()
+    file_end = fp.seek(0, 2)
+    interval = int((file_end - file_start) / n)
+    if interval == 0:
+        interval = 1
+
+    for repetition in range(repetitions):
+        offset_adjustment = randint(0, interval)
+        for base_offset in range(file_start, file_end, interval):
+            offset = base_offset + offset_adjustment
+            fp.seek(offset)
+            fp.readline()
+            line = fp.readline()
+            
+            if len(line) > 0 and line[-1] == '\n':
+                yield line
